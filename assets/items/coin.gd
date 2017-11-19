34e7529809f8fc4c62170ad6b36b23fae2b404c1
ObaniGemini <robin@pi-et-ro.net>
2017-11-19 17:07:35 +0100

    Add an anim for coin entering the screen

--- assets/items/coin.gd
+++ assets/items/coin.gd
@@ -1,5 +1,15 @@
 extends Area2D
 
+func _ready():
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	if get_pos().x - get_node("/root/main/knight").get_pos().x < 1000:
+		get_node("AnimationPlayer").play("spawn")
+		get_node("AnimationPlayer").connect("finished", get_node("AnimationPlayer"), "connect", ["finished", self, "queue_free"])
+		get_node("AnimationPlayer").connect("finished", get_node("AnimationPlayer"), "disconnect", ["finished", self, "queue_free"])
+		set_fixed_process(false)
+
 func _on_coin_body_enter( body ):
 	if body.get_name() == "knight":
 		get_node("/root/main").add_points(1,get_pos())
