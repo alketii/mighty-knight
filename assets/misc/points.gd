54d89ca5e2988a520088a944dce5fadcfd57aeaa
ObaniGemini <robin@pi-et-ro.net>
2017-11-19 17:52:48 +0100

    Cleanup points scene and add size changing depending on points

--- assets/misc/points.gd
+++ assets/misc/points.gd
@@ -1,17 +1,22 @@
-extends KinematicBody2D
+extends Node2D
 
-onready var tween = get_node("Tween")
 onready var label = get_node("label")
+onready var anim = get_node("AnimationPlayer").get_animation("bump").duplicate()
 
 func _ready():
 	var i = int(get_node("label").get_text())
 	if i > 10:
 		i = 10
+	
+	anim.track_set_key_value(0, 1, Vector2((5+i)/3, (5+i)/3))
+	get_node("AnimationPlayer").add_animation("bump_bis", anim)
+	get_node("AnimationPlayer").play("bump_bis")
+	
 	get_node("label").set("custom_colors/font_color", Color(1, 1.1-i*0.1, 1-i*0.1))
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	move(Vector2(0,-8))
-
-func _on_VisibilityNotifier2D_exit_screen():
-	queue_free()
+	if get_pos().y > -128:
+		set_pos(get_pos() + Vector2(0, -8))
+	else:
+		queue_free()
